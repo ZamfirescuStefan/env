94f7c3ef0e9e7747f4c3bea2552cfb816c611397
Stefan Zamfirescu <stefanzamfirescu@Stefans-MacBook-Air.local>
2025-08-04 12:09:32 +0300

    Added nvim support for local file

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -3,3 +3,9 @@ require('config.remaps')
 require('config.autocmds')
 require('config.lazy')
 require('config.commands')
+
+-- Source local machine config if it exists
+local local_config = vim.fn.expand("~/.nvim.local")
+if vim.fn.filereadable(local_config) == 1 then
+  dofile(local_config)
+end
